3c2348e82cc37599a5cf6fd1b2451f2578000779
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-02-02 06:38:46 -0800

    Add files via upload

---
+++ Data/IProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using InventorySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.DataAccess
+{
+    public interface IProductRepository
+    {
+
+
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int Id);
+        Product GetById(int id);
+        List<Product>GetAll();
+
+        bool IsProductExists(int id);
+
+    }
+    
+    
+}
